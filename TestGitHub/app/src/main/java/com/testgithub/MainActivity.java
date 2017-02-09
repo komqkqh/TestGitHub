345a9499fc89c574c13f29575d0638d26c70ab99
AFREECATV\komqkqh <komqkqh@afreecatv.com>
2017-02-09 16:23:46 +0900

    업로드이긔

---
+++ TestGitHub/app/src/main/java/com/testgithub/MainActivity.java
@@ -0,0 +1,16 @@
+package com.testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 깃헙 업로드 테스트
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
